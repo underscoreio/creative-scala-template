b6fd5468b6872e69c1661c45535d15794e51b493
Noel Welsh <noel@untyped.com>
2018-10-22 17:23:41 +0100

    Clear instructions on running the example
    Disambiguate the sbt `console` command from sbt itself.
    
    Closes #7

--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -4,9 +4,11 @@ import doodle.syntax._
 import doodle.jvm.Java2DFrame._
 import doodle.backend.StandardInterpreter._
 
-// To use this example, open the SBT console and type:
+// To use this example:
 //
-// Example.image.draw
+// 1. run `sbt`
+// 2. run the `console` command within `sbt`
+// 3. enter `Example.image.draw`
 object Example {
   val image = circle(10).fillColor(Color.red) on circle(20) on circle(30)
 
